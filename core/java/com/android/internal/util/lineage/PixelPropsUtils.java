e3dcc73c8c5a417adfa2e99c50f3f15ec85acc14
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2024-08-20 08:03:53 +0000

    PPU: spoof to caiman
    Change-Id: I661c791de12c7b464830b96cf0cdc1c94c2980e9

--- core/java/com/android/internal/util/lineage/PixelPropsUtils.java
+++ core/java/com/android/internal/util/lineage/PixelPropsUtils.java
@@ -412,16 +412,32 @@ public class PixelPropsUtils {
         }
     }
 
+    private static void setVersionFieldInt(String key, int value) {
+        try {
+            if (DEBUG) Log.d(TAG, "Defining prop " + key + " to " + value);
+            Field field = Build.VERSION.class.getDeclaredField(key);
+            field.setAccessible(true);
+            field.set(null, value);
+            field.setAccessible(false);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            Log.e(TAG, "Failed to set prop " + key, e);
+        }
+    }
+
     private static void spoofBuildGms() {
         // Alter build parameters to avoid hardware attestation enforcement
         setPropValue("BRAND", "google");
         setPropValue("MANUFACTURER", "Google");
-        setPropValue("DEVICE", "sailfish");
-        setPropValue("ID", "OPM1.171019.011");
-        setPropValue("FINGERPRINT", "google/sailfish/sailfish:8.1.0/OPM1.171019.011/4448085:user/release-keys");
-        setPropValue("MODEL", "Pixel");
-        setPropValue("PRODUCT", "sailfish");
-        setVersionFieldString("SECURITY_PATCH", "2017-12-05");
+        setPropValue("DEVICE", "caiman");
+        setPropValue("ID", "AD1A.240530.047.U1");
+        setPropValue("FINGERPRINT", "google/caiman/caiman:14/AD1A.240530.047.U1/12150698:user/release-keys");
+        setPropValue("MODEL", "Pixel 9 Pro");
+        setPropValue("PRODUCT", "caiman");
+        setPropValue("TYPE", "user");
+        setPropValue("TAGS", "release-keys");
+        setVersionFieldString("INCREMENTAL", "12150698");
+        setVersionFieldString("SECURITY_PATCH", "2024-08-05");
+        setVersionFieldInt("DEVICE_INITIAL_SDK_INT", 34);
     }
 
     private static boolean isCallerSafetyNet() {
